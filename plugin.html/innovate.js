04e0c899a0b08ca6833e45cf3a089d7c3cbe060b
sohaya1149 <73849332+sohaya1149@users.noreply.github.com>
2020-11-12 22:03:20 +0530

    Hamsa
     a chrome extension, that will activate when consumers are on a shopping website. While people are searching for the commodities they want on online shopping platforms the plugin will try to find the same items but in handicrafts, providing a comparison. Eg: if I am looking for a towel on amazon the plugin should be able to find the word “towel” as a product description within its database and provide me with a handicraft option of a towel.
    v

---
+++ plugin.html/innovate.js
@@ -0,0 +1,13 @@
+function openLink(){
+  window.open("https://www.sohayainderkaur.com/")
+  //addEventListener('click', function(){
+
+  //  var body = document.body;
+  //  var newImage = document.createElement('img');
+
+  //  newImage.src = 'flowers.png';
+
+  //  body.insertBefore(newImage, body.firstChild);
+
+  })
+//})();
